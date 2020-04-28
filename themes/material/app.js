63ee82a7b9ffc4524c4a360cf08e3a0b18f1f69f
东子 <xhwsd@qq.com>
2020-04-29 04:43:38 +0800

    尝试解决无法展示音频

--- themes/material/app.js
+++ themes/material/app.js
@@ -144,8 +144,8 @@ function list_files(path,files){
                     markdown("#head_md",data);
                 });
             }
-            var ext = p.split('.').pop();
-            if("|html|php|css|go|java|js|json|txt|sh|md|mp4|webm|avi|bmp|jpg|jpeg|png|gif|m4a|mp3|wav|ogg|mpg|mpeg|mkv|rm|rmvb|mov|wmv|asf|ts|flv|".indexOf(`|${ext.toLowerCase()}|`) >= 0){
+            var ext = p.split('.').pop().toLowerCase();
+            if("|html|php|css|go|java|js|json|txt|sh|md|mp4|webm|avi|bmp|jpg|jpeg|png|gif|m4a|mp3|wav|ogg|mpg|mpeg|mkv|rm|rmvb|mov|wmv|asf|ts|flv|".indexOf(`|${ext}|`) >= 0){
 	            p += "?a=view";
 	            c += " view";
             }
