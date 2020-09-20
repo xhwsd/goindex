3401fb304257b4c723ae895cbd2fb7f0c523aff5
xhwsd <xhwsd@qq.com>
2020-09-20 19:12:59 +0800

    更新

--- themes/material/app.js
+++ themes/material/app.js
@@ -144,7 +144,7 @@ function list_files(path, files) {
                 });
             }
             var ext = p.split('.').pop().toLowerCase();
-            if ("|html|php|css|go|java|js|json|txt|sh|md|mp4|webm|avi|bmp|jpg|jpeg|png|gif|m4a|mp3|wav|ogg|mpg|mpeg|mkv|rm|rmvb|mov|wmv|asf|ts|flv|".indexOf(`|${ext}|`) >= 0){
+            if ("|html|php|css|go|java|js|json|txt|sh|md|mp4|webm|avi|bmp|jpg|jpeg|png|gif|m4a|mp3|wav|ogg|mpg|mpeg|mkv|rm|rmvb|mov|wmv|asf|ts|flv|".indexOf(`|${ext}|`) >= 0) {
 	            p += "?a=view";
 	            c += " view";
             }
