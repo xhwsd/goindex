2697e9467b8cba3405b4622549c67f6d9d5cce63
东子 <xhwsd@qq.com>
2020-09-20 15:14:31 +0800

    Update app.js
    升级 ace

--- themes/material/app.js
+++ themes/material/app.js
@@ -231,8 +231,8 @@ function file_code(path){
 </div>
 <a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 
-<script src="https://cdn.staticfile.org/ace/1.4.7/ace.js"></script>
-<script src="https://cdn.staticfile.org/ace/1.4.7/ext-language_tools.js"></script>
+<script src="https://cdn.staticfile.org/ace/1.4.9/ace.min.js"></script>
+<script src="https://cdn.staticfile.org/ace/1.4.9/ext-language_tools.min.js"></script>
 	`;
 	$('#content').html(content);
 	
